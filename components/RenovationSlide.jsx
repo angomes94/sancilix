d2e6309b2b2da74fb4613a132a07311085ccecaa
angomes94 <angomes.94@gmail.com>
2023-07-18 10:51:31 +0100

    new padding on renovations

--- components/RenovationSlide.jsx
+++ components/RenovationSlide.jsx
@@ -17,32 +17,31 @@ export default function RenovationSlide({ imagePairs }) {
 
 
   return (
-    <div className=" w-full h-screen md:p-10">
+    
       <Swiper
         style={{ "--swiper-navigation-color": "#172554", "--swiper-pagination-color": "#172554" }}
         loop={true}
         navigation={true}
         modules={[FreeMode, Navigation]}
-        className="w-full h-full "
+        className="w-full h-screen "
 
       >
         {imagePairs.map((pair, index) => (
           <SwiperSlide key={index}>
-            <div className="w-full h-full ">
-              <div className="w-full h-full flex flex-col md:px-20">
+            <>
+              <div className="w-full h-full flex flex-col pb-10 md:px-20">
                 <div className="w-full h-1/2 p-2 md:p-10">
                   <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
-                  <img src={urlFor(pair.before).fit('clip').width(2000).height(1000).url()} className="w-full h-full object-contain cursor-zoom-in duration-300 hover:scale-150" />
+                  <img src={urlFor(pair.before).fit('clip').width(2000).height(1000).url()} className="w-full h-full object-contain" />
                 </div>
                 <div className="w-full h-1/2 p-2 md:p-10">
                   <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideAfter}</h1>
-                  <img src={urlFor(pair.after).fit('clip').width(2000).height(1000).url()} className="w-full h-full object-contain cursor-zoom-in duration-300 hover:scale-150" />
+                  <img src={urlFor(pair.after).fit('clip').width(2000).height(1000).url()} className="w-full h-full object-contain" />
                 </div>
               </div>
-            </div>
+            </>
           </SwiperSlide>
         ))}
       </Swiper>
-    </div>
   );
 }
