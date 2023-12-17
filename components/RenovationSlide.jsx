e1bc6724423dd60d7d9a971ad0735fd66127bf2a
angomes94 <angomes.94@gmail.com>
2023-12-17 20:07:58 +0000

    bug mobile images disapear on cube efect

--- components/RenovationSlide.jsx
+++ components/RenovationSlide.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
@@ -29,11 +29,11 @@ export default function RenovationSlide({ imagePairs }) {
         {imagePairs.map((pair, index) => (
           <SwiperSlide key={index}> 
               <div className="w-full h-full flex flex-col justify-center items-center pb-10 md:px-20">
-                <div className="w-full h-[50vh] max-w-[1000px] p-2 md:p-10">
+                <div className="w-full h-[50vh] p-2 md:p-10">
                   <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
                   <img src={urlFor(pair.before).fit('clip').width(2000).height(1000).url()} className="w-full h-full object-contain" />
                 </div>
-                <div className="w-full h-[50vh] max-w-[1000px] p-2 md:p-10">
+                <div className="w-full h-[50vh] p-2 md:p-10">
                   <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideAfter}</h1>
                   <img src={urlFor(pair.after).fit('clip').width(2000).height(1000).url()} className="w-full h-full object-contain" />
                 </div>
