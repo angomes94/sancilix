bd5f4ae2d3559696c2017d195d18c2949fa88783
angomes94 <angomes.94@gmail.com>
2023-12-30 17:41:45 +0000

    removed fixed width and height of the images Slide

--- components/RenovationSlide.jsx
+++ components/RenovationSlide.jsx
@@ -1,15 +1,14 @@
 "use client";
-import React, { useContext } from "react";
+import React, { useContext} from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/free-mode";
 import "swiper/css/navigation";
-import "swiper/css/thumbs";
-import { FreeMode, Navigation, Thumbs } from "swiper";
+import { Navigation } from "swiper";
 import { urlFor } from "@/lib/sanityClient";
 import { LanguageContext } from "@/app/context/languangeContext";
 
 export default function RenovationSlide({ imagePairs }) {
+
 
 
   const { translations } = useContext(LanguageContext);
@@ -17,27 +16,25 @@ export default function RenovationSlide({ imagePairs }) {
 
 
   return (
-    
+
       <Swiper
         style={{ "--swiper-navigation-color": "#172554", "--swiper-pagination-color": "#172554" }}
-        loop={true}
         navigation={true}
-        modules={[FreeMode, Navigation]}
-        className="w-full h-[100vh] "
-
+        modules={[Navigation]}
+        className="w-full h-screen "
       >
         {imagePairs.map((pair, index) => (
-          <SwiperSlide key={index}> 
-              <div className="w-full h-full flex flex-col justify-center items-center pb-10 md:px-20">
-                <div className="w-full h-[50vh] p-2 md:p-10">
-                  <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
-                  <img src={urlFor(pair.before).fit('clip').width(2000).height(1000).url()} className="w-full h-full object-contain" />
-                </div>
-                <div className="w-full h-[50vh] p-2 md:p-10">
-                  <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideAfter}</h1>
-                  <img src={urlFor(pair.after).fit('clip').width(2000).height(1000).url()} className="w-full h-full object-contain" />
-                </div>
+          <SwiperSlide key={index}>
+            <div className="w-full h-screen flex flex-col justify-center items-center pb-10 md:px-20">
+              <div className="w-full h-1/2 p-2 md:p-10">
+                <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
+                <img src={urlFor(pair.before).fit('clip').url()} className="w-full h-full object-contain" />
               </div>
+              <div className="w-full h-1/2 p-2 md:p-10">
+                <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideAfter}</h1>
+                <img src={urlFor(pair.after).fit('clip').url()} className="w-full h-full object-contain" />
+              </div>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
