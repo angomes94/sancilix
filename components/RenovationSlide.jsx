5f9cbf1f83cde93426ab4aaa84687005d1f90bf3
angomes94 <angomes.94@gmail.com>
2023-12-30 21:24:07 +0000

    image landscape detection to change flex box orientation

--- components/RenovationSlide.jsx
+++ components/RenovationSlide.jsx
@@ -1,42 +1,50 @@
 "use client";
-import React, { useContext} from "react";
+import React, { useContext, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
 import { urlFor } from "@/lib/sanityClient";
+import {getImageDimensions} from '@sanity/asset-utils'
 import { LanguageContext } from "@/app/context/languangeContext";
 
 export default function RenovationSlide({ imagePairs }) {
 
-
-
   const { translations } = useContext(LanguageContext);
+
+
+  const isImageLandscape = (imageUrl) => {
+
+    const { width, height } = getImageDimensions({ url: imageUrl });
+
+    return width > height ? true : false;
+
+  }
 
 
 
   return (
 
-      <Swiper
-        style={{ "--swiper-navigation-color": "#172554", "--swiper-pagination-color": "#172554" }}
-        navigation={true}
-        modules={[Navigation]}
-        className="w-full h-screen "
-      >
-        {imagePairs.map((pair, index) => (
-          <SwiperSlide key={index}>
-            <div className="w-full h-screen flex flex-col justify-center items-center pb-10 md:px-20">
-              <div className="w-full h-1/2 p-2 md:p-10">
-                <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
-                <img src={urlFor(pair.before).fit('clip').url()} className="w-full h-full object-contain" />
-              </div>
-              <div className="w-full h-1/2 p-2 md:p-10">
-                <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideAfter}</h1>
-                <img src={urlFor(pair.after).fit('clip').url()} className="w-full h-full object-contain" />
-              </div>
+    <Swiper
+      style={{ "--swiper-navigation-color": "#172554", "--swiper-pagination-color": "#172554" }}
+      navigation={true}
+      modules={[Navigation]}
+      className="w-full h-screen "
+    >
+      {imagePairs.map((pair, index) => (
+        <SwiperSlide key={index}>
+          <div className={`w-full h-screen flex justify-center items-center pb-10 md:px-20 ${isImageLandscape(urlFor(pair.before).url()) ? "flex-col" : "flex-row"}`}>              
+          <div className={`${isImageLandscape(urlFor(pair.before).url()) ? "w-full h-[50vh]  " : "w-[50vw] h-full"} w-full h-[50vh] p-2 md:p-10`}>
+            <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
+            <img src={urlFor(pair.before).fit('clip').url()} className="w-full h-full object-contain" />
+          </div>
+            <div className={`${isImageLandscape(urlFor(pair.before).url()) ? "w-full h-[50vh]  " : "w-[50vw] h-full"} p-2 md:p-10`}>
+              <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideAfter}</h1>
+              <img src={urlFor(pair.after).fit('clip').url()} className="w-full h-full object-contain" />
             </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
