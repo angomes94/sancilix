4a9956512ae0ab6c3cb23e9c4dd6e8192f878fb1
angomes94 <angomes.94@gmail.com>
2023-12-30 21:57:47 +0000

    bug3

--- components/RenovationSlide.jsx
+++ components/RenovationSlide.jsx
@@ -5,7 +5,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
 import { urlFor } from "@/lib/sanityClient";
-import {getImageDimensions} from '@sanity/asset-utils'
+import { getImageDimensions } from '@sanity/asset-utils'
 import { LanguageContext } from "@/app/context/languangeContext";
 
 export default function RenovationSlide({ imagePairs }) {
@@ -24,27 +24,28 @@ export default function RenovationSlide({ imagePairs }) {
 
 
   return (
-
-    <Swiper
-      style={{ "--swiper-navigation-color": "#172554", "--swiper-pagination-color": "#172554" }}
-      navigation={true}
-      modules={[Navigation]}
-      className="w-[100vw] h-[100vh] "
-    >
-      {imagePairs.map((pair, index) => (
-        <SwiperSlide key={index}>
-          <div className={`w-full h-screen flex justify-center items-center pb-10 md:px-20 ${isImageLandscape(urlFor(pair.before).url()) ? "flex-col" : "flex-row"}`}>              
-          <div className={`${isImageLandscape(urlFor(pair.before).url()) ? "w-[100vw] h-[50vh]  " : "w-[50vw] h-[100vh]"} p-2 md:p-10`}>
-            <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
-            <img src={urlFor(pair.before).url()} className="w-full h-full object-contain" />
-          </div>
-            <div className={`${isImageLandscape(urlFor(pair.before).url()) ? "w-[100vw] h-[50vh]  " : "w-[50vw] h-[100vh]"} p-2 md:p-10`}>
-              <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideAfter}</h1>
-              <img src={urlFor(pair.after).url()} className="w-full h-full object-contain" />
+    <>
+      <Swiper
+        style={{ "--swiper-navigation-color": "#172554", "--swiper-pagination-color": "#172554" }}
+        navigation={true}
+        modules={[Navigation]}
+        className="w-full h-[100vh] "
+      >
+        {imagePairs.map((pair, index) => (
+          <SwiperSlide key={index}>
+            <div className={`w-full h-screen flex justify-center items-center pb-10 md:px-20 ${isImageLandscape(urlFor(pair.before).url()) ? "flex-col" : "flex-row"}`}>
+              <div className={`${isImageLandscape(urlFor(pair.before).url()) ? "w-full h-[50vh]  " : "w-[50vw] h-full"} p-2 md:p-10`}>
+                <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
+                <img src={urlFor(pair.before).url()} className="w-full h-full object-contain" />
+              </div>
+              <div className={`${isImageLandscape(urlFor(pair.before).url()) ? "w-full h-[50vh]  " : "w-[50vw] h-full"} p-2 md:p-10`}>
+                <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideAfter}</h1>
+                <img src={urlFor(pair.after).url()} className="w-full h-full object-contain" />
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 }
