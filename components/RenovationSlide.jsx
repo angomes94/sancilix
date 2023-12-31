02a9476dd42a7440e5996deb364d080c30cc0415
angomes94 <angomes.94@gmail.com>
2023-12-31 12:07:32 +0000

    more testing stuff

--- components/RenovationSlide.jsx
+++ components/RenovationSlide.jsx
@@ -34,11 +34,11 @@ export default function RenovationSlide({ imagePairs }) {
         {imagePairs.map((pair, index) => (
           <SwiperSlide key={index}>
             <div className={`w-full flex justify-center items-center pb-10 p-2 md:px-20 ${isImageLandscape(urlFor(pair.before).url()) ? "flex-col" : "flex-row"}`}>
-              <div className="  flex-1 justify-center items-center p-2 md:p-5 ">
+              <div className=" flex flex-col flex-1 justify-center items-center p-2 md:p-5 ">
                 <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
                 <img src={urlFor(pair.before).fit('max').url()} alt="before" className=" rounded-xl object-contain" />
               </div>
-              <div className=" flex-1 justify-center items-center p-2 md:p-5 ">
+              <div className=" flex flex-col flex-1 justify-center items-center p-2 md:p-5 ">
                 <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideAfter}</h1>
                 <img src={urlFor(pair.after).fit('max').url()} alt="after" className=" rounded-xl object-contain" />
               </div>
