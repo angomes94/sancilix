845eb79b1b6c46e782347cc3bf1c5960f7b8a5b7
angomes94 <angomes.94@gmail.com>
2023-12-31 11:33:39 +0000

    server bug testing solution

--- components/RenovationSlide.jsx
+++ components/RenovationSlide.jsx
@@ -33,14 +33,14 @@ export default function RenovationSlide({ imagePairs }) {
       >
         {imagePairs.map((pair, index) => (
           <SwiperSlide key={index}>
-            <div className={`w-full h-[100vh] flex justify-center items-center pb-10 px-20 ${isImageLandscape(urlFor(pair.before).url()) ? "flex-col" : "flex-row"}`}>
-              <div className={`${isImageLandscape(urlFor(pair.before).url()) ? "w-full h-[50vh]  " : "w-[50vw] h-full"} p-2 md:p-10`}>
+            <div className={`w-full flex justify-center items-center pb-10 p-2 md:px-20 ${isImageLandscape(urlFor(pair.before).url()) ? "flex-col" : "flex-row"}`}>
+              <div className="flex-1 p-10">
                 <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
-                <img src={urlFor(pair.before).fit('max').url()} alt="before" className="w-full h-full object-contain" />
+                <img src={urlFor(pair.before).fit('max').url()} alt="before" className=" object-contain" />
               </div>
-              <div className={`${isImageLandscape(urlFor(pair.before).url()) ? "w-full h-[50vh]  " : "w-[50vw] h-full"} p-2 md:p-10`}>
+              <div className="flex-1 p-10">
                 <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideAfter}</h1>
-                <img src={urlFor(pair.after).fit('max').url()} alt="after" className="w-full h-full object-contain" />
+                <img src={urlFor(pair.after).fit('max').url()} alt="after" className=" object-contain" />
               </div>
             </div>
           </SwiperSlide>
