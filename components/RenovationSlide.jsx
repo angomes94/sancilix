ee05d326c7f3551a2d9701d0e15452243ecfb49d
angomes94 <angomes.94@gmail.com>
2024-01-01 22:17:22 +0000

    max w added and h too

--- components/RenovationSlide.jsx
+++ components/RenovationSlide.jsx
@@ -7,6 +7,7 @@ import { Navigation } from "swiper";
 import { urlFor } from "@/lib/sanityClient";
 import { getImageDimensions } from '@sanity/asset-utils'
 import { LanguageContext } from "@/app/context/languangeContext";
+import { filterProps } from "framer-motion";
 
 export default function RenovationSlide({ imagePairs }) {
 
@@ -24,23 +25,23 @@ export default function RenovationSlide({ imagePairs }) {
 
 
   return (
-    <div className="md:mx-10">
+    <div className=" md:mx-10">
       <Swiper
         style={{ "--swiper-navigation-color": "#172554", "--swiper-pagination-color": "#172554" }}
         navigation={true}
         modules={[Navigation]}
-        className=" w-full  "
+        className=" w-full   "
       >
         {imagePairs.map((pair, index) => (
           <SwiperSlide key={index} >
             <div className={` flex justify-center items-center pb-10 p-2 md:px-20  ${isImageLandscape(urlFor(pair.before).url()) ? "flex-col" : "flex-row"}`}>
-              <div className=" flex flex-col flex-1 justify-center items-center max-w-[1500px] p-2 md:p-5  ">
+              <div className=" flex flex-col flex-1 justify-center items-center  p-2 md:p-5  ">
                 <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
-                <img src={urlFor(pair.before).url()} alt="before" className=" rounded-xl object-contain "/>
+                <img src={urlFor(pair.before).url()} alt="before" className=" rounded-xl object-contain max-w-[1000px] max-h-[800px] "/>
               </div>
               <div className=" flex flex-col flex-1 justify-center items-center max-w-[1500px] p-2 md:p-5 ">
                 <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideAfter}</h1>
-                <img src={urlFor(pair.after).url()} alt="after" className=" rounded-xl object-contain" />
+                <img src={urlFor(pair.after).url()} alt="after" className=" rounded-xl object-contain max-w-[1000px] max-h-[800px]" />
               </div>
             </div>
           </SwiperSlide>
