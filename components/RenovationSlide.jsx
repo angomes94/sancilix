fa43eba674de18a15f482ec8e9890b695fea7eea
angomes94 <angomes.94@gmail.com>
2024-01-01 20:10:42 +0000

    images renovation last change

--- components/RenovationSlide.jsx
+++ components/RenovationSlide.jsx
@@ -24,16 +24,16 @@ export default function RenovationSlide({ imagePairs }) {
 
 
   return (
-    <div>
+    <div className="md:mx-10">
       <Swiper
         style={{ "--swiper-navigation-color": "#172554", "--swiper-pagination-color": "#172554" }}
         navigation={true}
         modules={[Navigation]}
-        className=" w-full "
+        className=" w-full  "
       >
         {imagePairs.map((pair, index) => (
           <SwiperSlide key={index} >
-            <div className={`w-full h[90vh] flex justify-center items-center pb-10 p-2 md:px-20 ${isImageLandscape(urlFor(pair.before).url()) ? "flex-col" : "flex-row"}`}>
+            <div className={` flex justify-center items-center pb-10 p-2 md:px-20 ${isImageLandscape(urlFor(pair.before).url()) ? "flex-col" : "flex-row"}`}>
               <div className=" flex flex-col flex-1 justify-center items-center p-2 md:p-5  ">
                 <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
                 <img src={urlFor(pair.before).url()} alt="before" className=" rounded-xl object-contain"/>
