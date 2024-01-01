6e41893e800df9c5d363f33897bc25f62450e357
angomes94 <angomes.94@gmail.com>
2024-01-01 19:07:17 +0000

    images renovation

--- components/RenovationSlide.jsx
+++ components/RenovationSlide.jsx
@@ -34,11 +34,11 @@ export default function RenovationSlide({ imagePairs }) {
         {imagePairs.map((pair, index) => (
           <SwiperSlide key={index} >
             <div className={`w-full flex justify-center items-center pb-10 p-2 md:px-20 ${isImageLandscape(urlFor(pair.before).url()) ? "flex-col" : "flex-row"}`}>
-              <div className=" flex flex-col flex-1 justify-center items-center p-2 md:p-5  ">
+              <div className=" max-h-[50vh] flex flex-col flex-1 justify-center items-center p-2 md:p-5  ">
                 <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideBefore}</h1>
                 <img src={urlFor(pair.before).url()} alt="before" className={`${isImageLandscape(urlFor(pair.before).url()) ? "max-h-[40vh]" : "max-w-[40vw] max-h-[90vh]"}  rounded-xl object-contain `}/>
               </div>
-              <div className="flex flex-col flex-1 justify-center items-center p-2 md:p-5 ">
+              <div className="max-h-[50vh] flex flex-col flex-1 justify-center items-center p-2 md:p-5 ">
                 <h1 className="font-semibold text-xl text-center pb-5 ">{translations.slideAfter}</h1>
                 <img src={urlFor(pair.after).url()} alt="after" className={`${isImageLandscape(urlFor(pair.before).url()) ? "max-h-[40vh]" : "max-w-[40vw] max-h-[90vh]" }  rounded-xl object-contain `} />
               </div>
